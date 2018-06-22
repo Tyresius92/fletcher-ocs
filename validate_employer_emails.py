4e46e783da18afba8879fb907074d89d4bd03a52
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-22 11:18:07 -0400

    tweaked the printed messages

--- validate_employer_emails.py
+++ validate_employer_emails.py
@@ -14,19 +14,23 @@ del lines[0]
 i = 0
 
 for line in lines:
-    broken = line.split(sep=',')
-    email = broken[-1].replace('\n', '')
+    try: 
+        broken = line.split(sep=',')
+        email = broken[-1].replace('\n', '')
 
-    i += 1
-    if i % 100 == 0:
-        print(str(i) + " emails processed")
-        print("most recent email: " + email)
+        i += 1
+        if i % 100 == 0:
+            print(str(i) + " emails processed")
+            print("most recent email: " + email)
 
-    is_valid = validate_email(email, verify=True)
+        is_valid = validate_email(email, verify=True)
 
-    if is_valid:
-        output.write(line.replace('\n', '') + ", Valid\n")
-    else:
-        output.write(line.replace('\n', '') + ", Invalid\n")
+        if is_valid:
+            output.write(line.replace('\n', '') + ", Valid\n")
+        else:
+            output.write(line.replace('\n', '') + ", Invalid\n")
+    except:
+        output.write(line.replace('\n', '') + ", Error\n")
+        continue
 
 output.close()
